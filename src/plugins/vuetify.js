c813567f2429c201540005ac3a9b7343e88a366e
Alison Moura <alison.oghino@gmail.com>
2020-12-15 18:06:48 -0400

    Lint for ESLint + Prettier

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-import pt from "vuetify/lib/locale/pt";
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import pt from 'vuetify/lib/locale/pt'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
   theme: {
@@ -12,18 +12,18 @@ export default new Vuetify({
     },
     themes: {
       light: {
-        primary: "#ee44aa",
-        secondary: "#424242",
-        accent: "#82B1FF",
-        error: "#FF5252",
-        info: "#2196F3",
-        success: "#4CAF50",
-        warning: "#FFC107"
+        primary: '#ee44aa',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
       }
     }
   },
   lang: {
     locales: { pt },
-    current: "pt"
+    current: 'pt'
   }
-});
+})
